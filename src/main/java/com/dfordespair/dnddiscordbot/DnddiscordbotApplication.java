f52adb48208cfcc9f20c4525c2bbb5f39cf07bfb
DForDespair <aryan@amoghaddam.com>
2023-08-06 01:58:01 -0400

    initial commit

---
+++ src/main/java/com/dfordespair/dnddiscordbot/DnddiscordbotApplication.java
@@ -0,0 +1,13 @@
+package com.dfordespair.dnddiscordbot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DnddiscordbotApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DnddiscordbotApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/dfordespair/dnddiscordbot/DnddiscordbotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dfordespair.dnddiscordbot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DnddiscordbotApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
